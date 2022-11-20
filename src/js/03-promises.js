a3f48851030c59ab19014e6f9d9f61b1314806d6
Yurii Oborskiy <yuriioborskiy@gmail.com>
2022-11-20 11:22:52 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import Notiflix from 'notiflix';
+
+const createPromiseFormEl = document.querySelector('.form');
+const delayEl = createPromiseFormEl.elements.delay;
+const stepEl = createPromiseFormEl.elements.step;
+const amountEl = createPromiseFormEl.elements.amount;
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const onCreatePromiseFormSubmit = event => {
+  event.preventDefault();
+
+  for (let i = 0; i < Number(amountEl.value); i += 1) {
+    createPromise(i + 1, Number(delayEl.value) + i * Number(stepEl.value))
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+};
+
+createPromiseFormEl.addEventListener('submit', onCreatePromiseFormSubmit);
