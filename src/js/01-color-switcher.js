7ceb2ecbaa3e9f9d29c51b027c5801b9f33096bf
Yurii Oborskiy <yuriioborskiy@gmail.com>
2022-11-20 07:11:38 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+stopBtnEl.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const colorSwitch = () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+};
+
+const onStartBtnClick = () => {
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+  colorSwitch();
+  timerId = setInterval(colorSwitch, 1000);
+};
+
+const onStopBtnClick = () => {
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+  clearInterval(timerId);
+};
+
+startBtnEl.addEventListener('click', onStartBtnClick);
+stopBtnEl.addEventListener('click', onStopBtnClick);
